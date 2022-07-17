0c6a834007dd8b0484efbdd7ad59f4b0ded3f887
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-17 09:19:18 +0100

    feat(core): add the skeleton of the site header

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<header>
+    <h1>Watch Shop<span class="city">LONDON</span></h1>
+</header>
+
+<slot></slot>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    h1 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
