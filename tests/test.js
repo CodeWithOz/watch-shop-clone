eadcc2ddb8d6459b4abe18ba964666ceac85f02a
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-17 10:43:39 +0100

    test(core): test presence of hamburger and cart icons

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,18 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	const headerTextSelector = 'header h1';
-	expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('watch');
-	expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('shop');
-	expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('london');
+test.describe('index page header', () => {
+	test('has expected h1', async ({ page }) => {
+		await page.goto('/');
+		const headerTextSelector = 'header h1';
+		expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('watch');
+		expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('shop');
+		expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('london');
+	});
+
+	test('has hamburger and cart icons', async ({ page }) => {
+		await page.goto('/');
+		console.log('count object', await page.locator('header img.hamburger').count());
+		expect(await page.locator('header img.hamburger').count()).toEqual(1);
+		expect(await page.locator('header img.cart').count()).toEqual(1);
+	});
 });
