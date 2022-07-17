d7c60961d5378eefde9fee0116f28a21e1a6f40d
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-17 10:12:12 +0100

    feat(core): add custom font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,9 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="/global.css" rel="stylesheet">
+</svelte:head>
+
 <header>
     <h1>Watch Shop<span class="city">LONDON</span></h1>
 </header>
