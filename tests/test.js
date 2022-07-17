c320fe0bf5b5157ee891ce4916f7be07aed7b1f0
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-17 14:36:43 +0100

    fix(test): stop testing for visibility based on viewport size

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-/**
- * @type {import('@playwright/test').ViewportSize}
- */
-const mobileViewportDims = {
-	// slighty bigger than pixel XL size, to cover that and smaller mobile devices
-	height: 830,
-	width: 420
-};
-
-/**
- * @type {import('@playwright/test').ViewportSize}
- */
-const defaultViewportDims = {
-	// corresponds to mid-sized pc display
-	height: 720,
-	width: 1280
-};
-
 test.describe('index page header', () => {
-	test('has expected h1', async ({ browser }) => {
-		const page = await browser.newPage();
-		await page.setViewportSize(mobileViewportDims);
+	test('has expected h1', async ({ page }) => {
 		await page.goto('/');
 		const headerTextSelector = 'header h1';
 		expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('watch');
@@ -29,18 +9,9 @@ test.describe('index page header', () => {
 		expect((await page.textContent(headerTextSelector)).toLowerCase()).toContain('london');
 	});
 
-	test('has cart icon', async ({ page }) => {
+	test('has hamburger and cart icons', async ({ page }) => {
 		await page.goto('/');
-		expect(await page.locator('header img.cart').count()).toEqual(1);
-	});
-
-	test('has hamburger icon on mobile only', async ({ browser }) => {
-		const page = await browser.newPage();
-		await page.setViewportSize(mobileViewportDims);
-		await page.goto('/');
-		expect(await page.locator('header img.hamburger').isVisible()).toEqual(true);
-		await page.setViewportSize(defaultViewportDims);
-		await page.goto('/');
-		expect(await page.locator('header img.hamburger').isVisible()).toEqual(false);
+		expect(await page.locator('header .header__icon__cart img').count()).toEqual(1);
+		expect(await page.locator('header .header__icon__hamburger img').count()).toEqual(1);
 	});
 });
