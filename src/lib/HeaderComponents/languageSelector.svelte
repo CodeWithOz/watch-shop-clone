5e74f46725432db0d5f9fa6d22c4a561e3ecf429
Uche Ozoemena <ucheozoemena@gmail.com>
2022-07-18 08:55:49 +0100

    enhance(core): style the flag and dropdown properly

--- src/lib/HeaderComponents/languageSelector.svelte
+++ src/lib/HeaderComponents/languageSelector.svelte
@@ -3,7 +3,7 @@
         <select name="language" id="language">
             <option value="EN">EN</option>
         </select>
-        <div class="flagContainer"><img src="/flag-UK.svg" alt="flag" srcset="/flag-UK.svg 1x, /flag-UK@2x.svg 2x"></div>
+        <div class="flag__container"><img src="/flag-UK.svg" alt="flag" srcset="/flag-UK.svg 1x, /flag-UK@2x.svg 2x"></div>
     </div>
 </div>
 
@@ -13,12 +13,28 @@
         align-items: center;
         justify-content: flex-end;
         background-color: var(--site-white);
-        padding: 0 var(--outerPadLg);
+        padding: 0.25rem var(--outerPadLg);
     }
 
     .selector__container {
         display: flex;
         align-items: center;
         justify-content: center;
+    }
+
+    .selector__container select {
+        height: 1rem;
+        border: none;
+        background-color: var(--site-white);
+        margin-right: 0.25rem;
+    }
+
+    .flag__container {
+        height: 1.2rem;
+    }
+
+    .flag__container img {
+        height: 100%;
+        width: 100%;
     }
 </style>
